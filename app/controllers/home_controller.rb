fbabf8fc5b03c2b0facb8abfa5a7dfd1dccba662
jbitton <jbitton2013@fau.edu>
2016-11-18 14:52:09 -0500

    WORKING DBgit add --all!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,16 @@ class HomeController < ApplicationController
     @username = params["uname"]
     @password = params["pwd"]
 
+    u = User.where(:username => @username).first!
 
+    if u == nil
+      # error: invalid username
+    end
+
+    if u.password == @password
+      # login verified!
+    else
+      # invalid password
+    end
   end
 end
