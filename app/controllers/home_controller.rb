7fe450b5c02ceb555a70b9dffe1d32f663818076
morenoCami <cmcamilomoreno@gmail.com>
2016-11-19 23:02:45 -0500

    Basic method for finding courses needed for graduation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  helper_method :do_stuff
+
+  def find_remaining_courses_for_graduation
+    user = User.find(10)
+    major = Major.find(user.major_1)
+    coursesTaken = user.course_taken.split(",")
+    requirements = major.requirements.split(",")
+
+    remaining=requirements-coursesTaken
+
+    puts "\nCourses taken ARE:\n"+coursesTaken.to_s+"\n"
+    puts "\nMajor "+user.major_1.to_s+" requirements ARE:\n"+requirements.to_s+"\n\n"
+    puts "\nRemaining courses ARE:\n"+remaining.to_s+"\n\n"
+  end
+
   def index
   end
 end
