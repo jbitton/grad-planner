c0b7abb33907152d9d372b1397be6639c8696c99
jbitton <jbitton2013@fau.edu>
2016-11-30 09:15:24 -0500

    signup/login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,29 +2,48 @@ class HomeController < ApplicationController
   helper_method :do_stuff
 
   # this method should be moved to the generation_controller.rb once it is created
-  def find_remaining_courses_for_graduation
-    user = User.where(:username => @username).first
-    major = Major.find(user.major_1)
-    courses_taken = user.course_taken.split(",")
-    requirements = major.requirements.split(",")
+  #def find_remaining_courses_for_graduation
+   # user = User.where(:username => @username).first
+   # major = Major.find(user.major_1)
+   # courses_taken = user.course_taken.split(",")
+   # requirements = major.requirements.split(",")
 
-    remaining_courses=requirements-courses_taken
+   # remaining_courses=requirements-courses_taken
+  #end
+
+  def signin
+    unless params[:home].nil?
+      @username = params[:home][:username]
+      @password = params[:home][:password]
+
+      user = User.where(:username => @username).first
+
+      if user && user.password == @password
+        redirect_to '/login'
+      else
+        render partial: 'login', layout: '/layouts/home', locals: { invalid: true }
+      end
+    end
   end
 
+  def signup
+    unless params[:home].nil?
+      @username = params[:home][:username]
+      @password = params[:home][:password]
+      @confirm_password = params[:home][:confirm_password]
 
-  def index
-    @username = params["uname"]
-    @password = params["pwd"]
+      user = User.where(:username => @username).first
 
-    u = User.where(:username => @username).first
-
-    if u == nil
-      puts "Error"
-    else
-      if u.password == @password
-        puts "Login"
+      if user
+        render partial: 'signup', layout: '/layouts/home', locals: { invalid: 'Error: username already exists' }
+      elsif @password != @confirm_password
+        render partial: 'signup', layout: '/layouts/home', locals: { invalid: 'Error: passwords do not match' }
       else
-        puts "Bad credentials"
+        @user = User.new
+        @user.username = @username
+        @user.password = @password
+        # other attributes?
+        # go to new link afterwards
       end
     end
   end
