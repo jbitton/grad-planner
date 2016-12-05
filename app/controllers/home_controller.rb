e5a213a07ea7ace70f0710f38353fa26f3ae1698
jbitton <jbitton2013@fau.edu>
2016-12-05 15:54:59 -0500

    delete extraneous func

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,6 @@
 class HomeController < ApplicationController
   helper_method :do_stuff
-
-  # this method should be moved to the generation_controller.rb once it is created
-  #def find_remaining_courses_for_graduation
-   # user = User.where(:username => @username).first
-   # major = Major.find(user.major_1)
-   # courses_taken = user.course_taken.split(",")
-   # requirements = major.requirements.split(",")
-
-   # remaining_courses=requirements-courses_taken
-  #end
-
+  
   def signin
     unless params[:home].nil?
       @username = params[:home][:username]
