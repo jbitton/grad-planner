4edbf22b23713d053c71af5610af09eef9f65619
jbitton <jbitton2013@fau.edu>
2016-12-08 01:50:20 -0500

    changes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,15 @@ class SessionController < ApplicationController
       redirect_to '/home/signin'
       return
     end
+
+    classes_taken = UserSession.instance.get_user.courses_taken.split(',')
+    courses = []
+
+    classes_taken.each do |course|
+      courses.push(Course.where(:course_code => course).first)
+    end
+
+    render partial: 'dashboard', layout: '/layouts/session', locals: { active: 'dashboard', courses: courses }
   end
 
   def generate
@@ -23,18 +32,22 @@ class SessionController < ApplicationController
     potential_classes = cs_major
 
     classes_taken.each do |class_t|
-      potential_classes.reject! { |major_c| major_c. == class_t }
+      potential_classes.reject! { |major_c| major_c.course_code == class_t }
     end
 
     rejects = []
     potential_classes.each do |p_class|
-      if p_class[:prerequisites] && !(classes_taken.include?(p_class[:prerequisites]))
-        rejects.push(p_class)
+      if p_class.prerequisites != ''
+        p_class.prerequisites.split(',').each do |prereq|
+          unless classes_taken.include?(prereq)
+            rejects.push(p_class)
+          end
+        end
       end
     end
 
     rejects.each do |rejected|
-      potential_classes.reject! { |major_c| (major_c[:corequisites] == rejected[:name]) || (major_c == rejected) }
+      potential_classes.reject! { |major_c| (major_c.corequisites == rejected.course_code) || (major_c == rejected) }
     end
 
     render partial: 'generate', layout: '/layouts/session', locals: { active: 'generate', potential: potential_classes }
