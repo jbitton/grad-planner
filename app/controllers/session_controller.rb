9a72496683a16e018ecd1291ba122d48cbd661b0
lexlevi17 <sphota9@gmail.com>
2016-12-13 19:33:49 -0500

    change to normal rails session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,12 @@
 class SessionController < ApplicationController
   def dashboard
-    unless UserSession.instance.is_valid
+    unless session[:current_user_id]
       redirect_to '/home/signin'
       return
     end
 
-    classes_taken = UserSession.instance.get_user.courses_taken.split(',')
+    user = User.find(session[:current_user_id])
+    classes_taken = user.courses_taken.split(',')
     courses = []
 
     classes_taken.each do |course|
@@ -18,20 +19,21 @@ class SessionController < ApplicationController
   end
 
   def generate
-    unless UserSession.instance.is_valid
+    unless session[:current_user_id]
       redirect_to '/home/signin'
       return
     end
 
     unless params['demo-name'].nil? || params[:classtype].nil?
-      courses = Major.find(UserSession.instance.get_user.major_1).requirements.split(',')
+      user = User.find(session[:current_user_id])
+      courses = Major.find(user.major_1).requirements.split(',')
       cs_major = []
 
       courses.each do |course|
         cs_major.push(Course.where(:course_code => course).first)
       end
 
-      classes_taken = UserSession.instance.get_user.courses_taken.split(',')
+      classes_taken = user.courses_taken.split(',')
       potential_classes = cs_major
 
       classes_taken.each do |class_t|
@@ -53,15 +55,15 @@ class SessionController < ApplicationController
         potential_classes.reject! { |major_c| (major_c.corequisites == rejected.course_code) || (major_c == rejected) }
       end
 
-      if UserSession.instance.get_user.credits_taken < 90
+      if user.credits_taken < 90
         potential_classes.reject! { |major_c| major_c.course_code == 'EGN4950C' || major_c.course_code == 'COT4935' }
       end
 
-      if UserSession.instance.get_user.credits_taken < 60
+      if user.credits_taken < 60
         potential_classes.reject! { |major_c| major_c.course_code == 'CEN4214' }
       end
 
-      if UserSession.instance.get_user.credits_taken > 30
+      if user.credits_taken > 30
         potential_classes.reject! { |major_c| major_c.course_code == 'EGN1002' }
       end
 
@@ -72,7 +74,7 @@ class SessionController < ApplicationController
       recommended = potential_classes.dup
 
       unless recommended.count <= num_classes.to_i
-        major = UserSession.instance.get_user.major_1
+        major = user.major_1
         if core_or_elective == 'core'
           while recommended.count > num_classes.to_i && i < recommended.count
             if recommended[i].nil?
@@ -114,7 +116,7 @@ class SessionController < ApplicationController
   end
 
   def profile
-    unless UserSession.instance.is_valid
+    unless session[:current_user_id]
       redirect_to '/home/signin'
       return
     end
@@ -124,10 +126,12 @@ class SessionController < ApplicationController
         render partial: 'profile', layout: '/layouts/session', locals: { active: 'profile', invalid: true }
         return
       end
-      UserSession.instance.get_user.update_attribute(:first_name, params[:fname])
-      UserSession.instance.get_user.update_attribute(:last_name, params[:lname])
-      UserSession.instance.get_user.update_attribute(:major_1, params[:major].to_i)
-      UserSession.instance.get_user.update_attribute(:credits_taken, params[:ncredits].to_i)
+
+      user = User.find(session[:current_user_id])
+      user.update_attribute(:first_name, params[:fname])
+      user.update_attribute(:last_name, params[:lname])
+      user.update_attribute(:major_1, params[:major].to_i)
+      user.update_attribute(:credits_taken, params[:ncredits].to_i)
       courses_taken = []
       if params[:math] == 'mac1140' || params[:math] == 'mac2311'
         courses_taken.push('MAC1105')
@@ -138,27 +142,27 @@ class SessionController < ApplicationController
         courses_taken.push(course)
       end
 
-      UserSession.instance.get_user.update_attribute(:courses_taken, courses_taken.join(','))
+      user.update_attribute(:courses_taken, courses_taken.join(','))
     end
   end
 
   def settings
-    unless UserSession.instance.is_valid
+    unless session[:current_user_id]
       redirect_to '/home/signin'
       return
     end
-
+    user = User.find(session[:current_user_id])
     unless params[:new_username].nil?
-      if UserSession.instance.get_user.password == params[:password]
-        UserSession.instance.get_user.update_attribute(:username, params[:new_username])
+      if user.password == params[:password]
+        user.update_attribute(:username, params[:new_username])
       else
         render partial: 'settings', layout: '/layouts/session', locals: { active: 'settings', invalid: true }
       end
     end
 
     unless params[:new_password].nil?
-      if UserSession.instance.get_user.password == params[:old_password]
-        UserSession.instance.get_user.update_attribute(:password, params[:new_password])
+      if user.password == params[:old_password]
+        user.update_attribute(:password, params[:new_password])
       else
         render partial: 'settings', layout: '/layouts/session', locals: { active: 'settings', invalid: true }
       end
